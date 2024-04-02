e16348fc2b1831a051e1ee2d2240c411968f09b0
Vishal Kumar <47864536+TizonX@users.noreply.github.com>
2024-04-02 18:16:29 +0530

    feat: version update

--- component/index.js
+++ component/index.js
@@ -4,14 +4,28 @@ const ExpenseTracker = () => {
   const [expenses, setExpenses] = useState([]);
   const [inputAmount, setInputAmount] = useState("");
   const [selectedCategory, setSelectedCategory] = useState("");
-  const [customCategories, setCustomCategories] = useState([]);
+  const [customCategories, setCustomCategories] = useState([
+    "Food",
+    "Clothing",
+    "Glossary",
+  ]);
   const [errorMessage, setErrorMessage] = useState("");
   const listEndRef = useRef(null);
   const scrollButtonRef = useRef(null);
 
   useEffect(() => {
-    listEndRef.current.scrollIntoView({ behavior: "smooth" });
-  }, [expenses]);
+    const storedExpenses = JSON.parse(localStorage.getItem("expenses"));
+    if (storedExpenses) {
+      setExpenses(storedExpenses);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (expenses.length === 0) {
+      const parseExpenses = localStorage.getItem("expenses");
+      setExpenses(JSON.parse(parseExpenses));
+    }
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -35,9 +49,10 @@ const ExpenseTracker = () => {
         amount: formattedAmount,
         category: selectedCategory,
       };
-      setExpenses([expense, ...expenses]);
+      setExpenses([...expenses, expense]);
       setInputAmount("");
       setErrorMessage("");
+      localStorage.setItem("expenses", JSON.stringify([...expenses, expense]));
     } else {
       setErrorMessage("Please select a category.");
     }
@@ -47,6 +62,7 @@ const ExpenseTracker = () => {
     const updatedExpenses = [...expenses];
     updatedExpenses.splice(index, 1);
     setExpenses(updatedExpenses);
+    localStorage.setItem("expenses", JSON.stringify(updatedExpenses));
   };
 
   const handleKeyPress = (event) => {
@@ -98,53 +114,18 @@ const ExpenseTracker = () => {
 
   return (
     <div className="expense-tracker-container">
-      <h1>Expense Tracker</h1>
-      <div className="category-container">
-        <div className="categories">
-          {["Food", "Clothing", "Glossary", ...customCategories].map(
-            (category) => (
-              <span
-                key={category}
-                className={selectedCategory === category ? "selected" : ""}
-                onClick={() => handleSelectCategory(category)}
-              >
-                {category}
-                <button
-                  className="delete-category"
-                  onClick={() => handleDeleteCategory(category)}
-                >
-                  &times;
-                </button>
-              </span>
-            )
-          )}
-          <button
-            className="add-custom-category"
-            onClick={handleAddCustomCategory}
-          >
-            +
-          </button>
-        </div>
-      </div>
-      <div className="input-container">
-        <input
-          type="number"
-          placeholder="Enter Amount"
-          value={inputAmount}
-          onChange={(e) => setInputAmount(e.target.value)}
-          onKeyPress={handleKeyPress}
-        />
-        <button onClick={handleAddExpense}>Add Expense</button>
-      </div>
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
+      <h1 className="heading">Expense Tracker</h1>
       <div className="expenses-list">
         <ul>
           {expenses.map((expense, index) => (
-            <li key={index}>
-              <span onClick={() => handleAmountClick(expense.amount)}>
+            <li key={index} className="expense-item">
+              <span
+                className="amount"
+                onClick={() => handleAmountClick(expense.amount)}
+              >
                 {expense.amount}
               </span>
-              <span>{expense.category}</span>
+              <span className="list-category">{expense.category}</span>
               <button
                 className="delete-expense"
                 onClick={() => handleDeleteExpense(index)}
@@ -163,171 +144,54 @@ const ExpenseTracker = () => {
           &#9660;
         </button>
       </div>
-      <button
+      <div className="category-container">
+        <div className="categories">
+          {[...customCategories].map((category) => (
+            <span
+              key={category}
+              className={`category ${
+                selectedCategory === category ? "selected" : ""
+              }`}
+              onClick={() => handleSelectCategory(category)}
+            >
+              {category}
+              <button
+                className="delete-category"
+                onClick={() => handleDeleteCategory(category)}
+              >
+                &times;
+              </button>
+            </span>
+          ))}
+          <button
+            className="add-custom-category"
+            onClick={handleAddCustomCategory}
+          >
+            +
+          </button>
+        </div>
+      </div>
+      <div className="input-container">
+        <input
+          type="number"
+          className="amount-input"
+          placeholder="Enter Amount"
+          value={inputAmount}
+          onChange={(e) => setInputAmount(e.target.value)}
+          onKeyPress={handleKeyPress}
+        />
+        <button className="add-expense-button" onClick={handleAddExpense}>
+          +
+        </button>
+      </div>
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+      {/* <button
         className="show-category-expenses"
         onClick={handleShowCategoryExpenses}
       >
         Show Category Expenses
-      </button>
-
-      <style jsx>{`
-        .expense-tracker-container {
-          max-width: 600px;
-          margin: 0 auto;
-          padding: 20px;
-          border: 1px solid #ccc;
-          border-radius: 5px;
-          font-family: Arial, sans-serif;
-          position: relative;
-          background-color: #d3d3d3;
-        }
-
-        h1 {
-          text-align: center;
-          margin-bottom: 20px;
-        }
-
-        .category-container {
-          margin-bottom: 20px;
-        }
-
-        .categories {
-          display: flex;
-          overflow-x: auto;
-          margin-bottom: 10px;
-        }
-
-        .categories span {
-          position: relative;
-          cursor: pointer;
-          padding: 8px 15px;
-          background-color: #008080;
-          color: #fff;
-          border-radius: 20px;
-          margin-right: 10px;
-          font-size: 14px;
-          white-space: nowrap;
-        }
-
-        .categories span.selected {
-          background-color: #90e4c1;
-        }
-
-        .delete-category {
-          position: absolute;
-          top: 50%;
-          right: -8px;
-          transform: translateY(-50%);
-          background-color: transparent;
-          border: none;
-          color: #fff;
-          cursor: pointer;
-          font-size: 14px;
-        }
-
-        .add-custom-category {
-          cursor: pointer;
-          padding: 8px 15px;
-          background-color: #008080;
-          color: #fff;
-          border: none;
-          border-radius: 20px;
-          margin-left: 10px;
-          font-size: 16px;
-          position: fixed;
-          top: 20px;
-        }
-
-        .add-custom-category:hover {
-          background-color: #90e4c1;
-        }
-
-        .input-container {
-          display: flex;
-          align-items: center;
-          margin-bottom: 20px;
-        }
-
-        input[type="number"] {
-          padding: 10px;
-          border: 1px solid #ccc;
-          border-radius: 5px;
-          margin-right: 10px;
-          flex: 1;
-        }
-
-        button {
-          padding: 10px 20px;
-          background-color: #008080;
-          color: #fff;
-          border: none;
-          border-radius: 5px;
-          cursor: pointer;
-        }
-
-        button:hover {
-          background-color: #90e4c1;
-        }
-
-        .expenses-list {
-          max-height: 300px;
-          overflow-y: auto;
-          position: relative;
-        }
-
-        ul {
-          list-style-type: none;
-          padding: 0;
-        }
-
-        li {
-          display: flex;
-          justify-content: space-between;
-          border-bottom: 1px solid #ccc;
-          padding: 10px 0;
-        }
-
-        .delete-expense {
-          background-color: #008080;
-          color: #fff;
-          border: none;
-          border-radius: 50%;
-          cursor: pointer;
-        }
-
-        .delete-expense:hover {
-          background-color: #90e4c1;
-        }
-
-        .show-category-expenses {
-          display: block;
-          margin: 20px auto 0;
-        }
-
-        .error-message {
-          color: red;
-          margin-top: 5px;
-          font-size: 14px;
-        }
-
-        .scroll-to-bottom {
-          position: absolute;
-          bottom: calc(100% + 20px);
-          left: 50%;
-          transform: translateX(-50%);
-          background-color: #008080;
-          color: #fff;
-          border: none;
-          border-radius: 50%;
-          font-size: 20px;
-          cursor: pointer;
-          display: none;
-        }
-
-        .scroll-to-bottom:hover {
-          background-color: #90e4c1;
-        }
-      `}</style>
+      </button> */}
     </div>
   );
 };
