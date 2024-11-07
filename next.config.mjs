c2a6b8c814c12a458dd71fedc2d1f52af6860901
Vishal Kumar <vishalkumar@Solvatives-Laptop.local>
2024-11-07 13:12:52 +0530

    offline app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from 'next-pwa';
+
+const nextConfig = withPWA({
+  dest: 'public', // Destination for the generated service worker and PWA assets
+  disable: process.env.NODE_ENV === 'development', // Disable in development
+  // Additional PWA options (optional)
+});
 
 export default nextConfig;
